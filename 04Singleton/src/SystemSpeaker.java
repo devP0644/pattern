24207fb2e5a08885b75c7cba4d4347c64e1232f5
devP0644 <phg0644@naver.com>
2020-04-23 08:20:02 +0900

    create Singleton

---
+++ 04Singleton/src/SystemSpeaker.java
@@ -0,0 +1,25 @@
+public class SystemSpeaker {
+
+    static private SystemSpeaker instance;
+    private int volume = 5;
+
+    private SystemSpeaker() {
+
+    }
+
+    public static SystemSpeaker getInstance() {
+        if(instance == null) {
+            instance = new SystemSpeaker();
+        }
+
+        return instance;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+}
